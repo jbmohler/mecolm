49cd5e15135387655d55fdb5e1308d6a378232fe
Joel B. Mohler <joel@kiwistrawberry.us>
2021-11-07 18:51:54 -0500

    initial commit copied from yenot

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="mecolm",
+    version="0.1",
+    description="Tabular/Column structures with metadata (MEtadata COLuMns)",
+    author="Joel B. Mohler",
+    author_email="joel@kiwistrawberry.us",
+    url="https://bitbucket.org/jbmohler/mecolm",
+    packages=["mecolm"],
+)
